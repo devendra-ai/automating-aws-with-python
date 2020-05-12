ddf828be449b3ec574091cb0a9e9e69d1f6805ae
devendra-ai <58457653+devendra-ai@users.noreply.github.com>
2020-05-12 07:59:18 +0530

    Second day updation

---
+++ 09-webotron/AWS-scripting.py
@@ -0,0 +1,10 @@
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+
+s3 = session.resource('s3')
+'''for bucket in s3.buckets.all():
+    print(bucket)
+'''
+new_bucket = s3.create_bucket(Bucket='from-script')
+for bucket in s3.buckets.all():
+    print(bucket)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automating-aws-with-python
 Let us make some changes to this one!!!
+
+#Again some changes so that I remember Git
